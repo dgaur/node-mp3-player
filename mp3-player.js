7154efe0e1fce1d453282485ccbe7669a0878205
Dan Gaur <dan@dangaur.com>
2014-01-06 22:27:54 -0800

    Trivial little MP3 player

---
+++ mp3-player.js
@@ -0,0 +1,42 @@
+"use strict";
+
+//
+// Trivial little MP3 player for the CLI, for node.js
+//
+
+var async	= require("async");
+var fs		= require("fs");
+var lame	= require("lame");
+var speaker	= require("speaker");
+
+
+var playMP3 = function(file, callback) {
+	var decoder	= lame.Decoder();
+	var mp3		= fs.createReadStream(file);
+
+	// Pipe the file contents to the decoder
+	mp3.on('readable', function() {
+		mp3.pipe(decoder);
+	});
+
+	// Pipe the decoder output into the actual speaker interface
+	decoder.on('format', function(format) {
+		var s = new speaker(format);
+		s.on('flush', function() {
+			callback();
+		});
+
+		decoder.pipe(s);
+	});
+};
+
+
+//
+// Play each audio file given on the command-line, in turn
+//
+async.eachSeries(process.argv.slice(2), playMP3, function(error) {
+	if (error) {
+		console.error(error);
+	}
+});
+
